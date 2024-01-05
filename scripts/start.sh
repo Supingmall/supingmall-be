db7838c1f08f3c1e1857474aacb6f233ab5290ae
sihu <sihu89@daum.net>
2024-01-06 00:19:00 +0900

    git

--- scripts/start.sh
+++ scripts/start.sh
@@ -12,7 +12,7 @@ TIME_NOW=$(date +%c)
 echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
 cp $PROJECT_ROOT/susu/*.jar $JAR_FILE
 
-source ~/.bashrc
+source ~/.bash_profile
 
 echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
 nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
