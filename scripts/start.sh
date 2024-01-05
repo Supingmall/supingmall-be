be5f4fd5820845ed0aed415327b0577df48d14a6
sihu <sihu89@daum.net>
2024-01-06 00:43:10 +0900

    git

--- scripts/start.sh
+++ scripts/start.sh
@@ -6,6 +6,11 @@ JAR_FILE="/home/ubuntu/sihu/springBest/susu/spring.jar"
 APP_LOG="$PROJECT_ROOT/application.log"
 ERROR_LOG="$PROJECT_ROOT/error.log"
 DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
+# 직접 환경 변수 설정
+export DATABASE_USERNAME="root"
+export DATABASE_PASSWORD="12341234"
+export JWT_CODE="cheerupBE2"
+
 
 TIME_NOW=$(date +%c)
 
@@ -13,7 +18,7 @@ echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
 cp $PROJECT_ROOT/susu/*.jar $JAR_FILE
 
 #codedeploy bashrc를 읽어오지 못해 해당 파일 로드하게 작업
-source ~/.bash_profile
+#source ~/.bash_profile
 
 echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
 nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
